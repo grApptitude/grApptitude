5779498d70d72d2587953bb190d4eebf386b9c7b
Ivan Torrellas <ivantorrellas@gmail.com>
2015-07-23 22:12:59 -0500

    newGrapp API ready

--- client/app/newGrapp/newGrapp.controller.js
+++ client/app/newGrapp/newGrapp.controller.js
@@ -3,19 +3,20 @@
 angular.module('grapptitude')
   .controller('NewGrappCtrl', function ($scope, $http, Auth, User) {
     $scope.users = User.query();
+    $scope.getCurrentUser = Auth.getCurrentUser;
 
     $scope.newGrapp = {};
 
-      $scope.newGrapp = function() {
+      $scope.newGrapp = function(form) {
         if($scope.newGrapp === '') {
           return;
         }
         $http.post('/api/newGrapps', {
-          author_Id: $scope.getCurrentUser()._id,
+          author_Id: Auth.getCurrentUser._id,
           grapp: $scope.newGrapp.grapp,
-          timestamp: date.getDate(),
-          attachUrl: $scope.newGrapp.attachUrl,
-          tagedUser: $scope.newGrapp.tagedUser
+          timestamp: new Date(),
+          attachUrl: $input.val()
+          // tagedUser: $scope.newGrapp.tagedUser
         });
         $scope.newGrapp = '';
       };
